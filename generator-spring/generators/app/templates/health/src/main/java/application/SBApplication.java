5da3eaf32c3a45f06f38711b1d0927154d164da1
sjanuary <sjanuary@uk.ibm.com>
2018-02-21 09:51:29 +0000

    Merge with latest changes to master

--- generator-spring/generators/app/templates/health/src/main/java/application/SBApplication.java
+++ generator-spring/generators/app/templates/health/src/main/java/application/SBApplication.java
@@ -17,25 +17,27 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-{{/if}}
-{{/bluemix}}
+{{else}}
 {{#javametrics}}
 import org.springframework.context.annotation.ComponentScan;
 {{/javametrics}}
+{{/if}}
+{{/bluemix}}
 
 @SpringBootApplication
 {{#bluemix}}
 {{#if openApiServers.length}}
 @EnableSwagger2
-@ComponentScan(basePackages = { "io.swagger", "application" })
+@ComponentScan(basePackages = { "io.swagger", "application" {{#javametrics}}, "com.ibm.javametrics.spring"{{/javametrics}} })
+{{else}}
+{{#javametrics}}
+@ComponentScan(basePackages = {"application", "com.ibm.javametrics.spring"})
+{{/javametrics}}
 {{/if}}
 {{#mongodb}}
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 {{/mongodb}}
 {{/bluemix}}
-{{#javametrics}}
-@ComponentScan(basePackages = {"application", "com.ibm.javametrics.spring"})
-{{/javametrics}}
 public class SBApplication {
 
     {{#if bluemix.server.services.length}}
